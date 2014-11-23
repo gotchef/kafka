67a6874ec38747a46320b50ff9b57bf729ecd7ca
Franklin Wise <franklin@krave-n.com>
2014-11-23 09:51:05 -0800

    fix

--- recipes/common.rb
+++ recipes/common.rb
@@ -1,6 +1,11 @@
-node.default[:kafka][:full_version] = "#{node[:kafka][:version_scala]}-#{node[:kafka][:version]}"
-node.default[:kafka][:install_root_dir] = "#{node[:kafka][:install_root_dir]}"
-node.default[:kafka][:install_dir]  = "#{install_root_dir}/kafka_#{full_version}"
+full_version = "#{node[:kafka][:version_scala]}-#{node[:kafka][:version]}"
+install_root_dir= "#{node[:kafka][:install_root_dir]}"
+install_dir = "#{install_root_dir}/kafka_#{full_version}"
+
+node.default[:kafka][:install_dir] =  install_dir
+node.default[:kafka][:install_root_dir] = install_root_dir
+node.default[:kafka][:full_version] =full_version
+
 
 
 
