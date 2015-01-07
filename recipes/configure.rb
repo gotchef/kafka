28bba23add3a55fa8bd61d0686f6d4292dd5b4b7
Franklin Wise <franklin@krave-n.com>
2015-01-06 17:57:58 -0800

    kafka broker id from ip

--- recipes/configure.rb
+++ recipes/configure.rb
@@ -15,10 +15,15 @@ install_dir = "#{install_root_dir}/#{version_dir}"
 
 zookeeper_pairs = node[:kafka][:zookeeper_nodes]
 
-hostname = node['hostname']
+#hostname = node['hostname']
 # kafka-01, get the 01 part
-hostNumber = hostname.split('-')[1].strip.to_i
-node.default[:kafka][:broker_id] = hostNumber
+#hostNumber = hostname.split('-')[1].strip.to_i
+#node.default[:kafka][:broker_id] = hostNumber
+
+if node[:kafka][:broker_id].nil? || node[:kafka][:broker_id].empty?
+	node.default[:kafka][:broker_id] = node[:ipaddress].gsub(".","")
+end
+
 
 %w[server.properties consumer.properties producer.properties log4j.properties].each do |template_file|
 template "#{install_dir}/config/#{template_file}" do
