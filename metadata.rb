41febbb488e837417fe2442b992881597e31f91e
Franklin Wise <franklin@krave-n.com>
2015-02-09 18:26:38 -0800

    updated

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,6 @@ name				"kafka"
 maintainer        "Krave-n Inc."
 maintainer_email  "dev-ops@krave-n.com"
 license           "Copyright 2014 Krave-n Inc."
-description       "Intalls and configures a Kafka broker"
 version           "0.1"
 
 depends           "java"
